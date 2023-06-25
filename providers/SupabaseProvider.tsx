b3b1073655ac135c22b9c3af75e62c5badbc1f36
Arturo <chiefchurro@gmail.com>
2023-06-24 23:46:52 -0700

    setting up database

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Database } from "@/types_db";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+const SupabaseProvider = ({ children }: SupabaseProviderProps) => {
+  return <div>SupabaseProvider</div>;
+};
+
+export default SupabaseProvider;
